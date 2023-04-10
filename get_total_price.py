2a59728fa277c9aabb792b6b93ad0497f5e7f7c7
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-10 12:46:08 +0500

    x5

--- get_total_price.py
+++ get_total_price.py
@@ -7,6 +7,20 @@ def get_total_price(data:str)->float:
     returns:
         float: fruits total price
     """
-    return
+    ls = []
+    f = open(data).read().split("\n")
+    f.pop(0)
+    f.pop(-1)
+    for i in f:
+        n = i.split(",")
+        ls.append(float(n[1]))
+    
+    s = 0
+    h = 0
+    while s < len(ls):
+        h += ls[s]
+        s += 1
+    return round(h,2)
+print(get_total_price("fruits.csv"))
 
     
