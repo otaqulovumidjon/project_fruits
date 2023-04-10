4f8d0ba00d280ad0515bbd69ae5f065c153cce7c
otaqulovumidjon <umidjonotaqulov0003@gmail.com>
2023-04-10 10:22:48 +0500

    x3

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -7,7 +7,24 @@ def get_expensive_fruit(data: str) -> str:
     returns:
         str: name of the most expensive fruit
     """
+    ls = []
+    js = []
+    f = open(data).read().split("\n")
+    f.pop(0)
+    f.pop(-1)
+    for i in f:
+        n = i.split(",")
+        ls.append(float(n[1]))
+        js.append(n[0])
+
+    x = 0
+    y = 0
+    while x < len(ls):
+        if y < ls[x]:
+            y = ls[x]
+        x += 1
+    l = ls.index(y)
+    return js[l]
+print(get_expensive_fruit("fruits.csv"))
+
     # your code here
-    return  
-
-
